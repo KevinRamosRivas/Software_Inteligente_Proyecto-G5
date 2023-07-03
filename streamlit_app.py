77591e0fa4b90996ed6eb34a950c267f5b530acd
Kevin Ramos Rivas <80496624+KevinRamosRivas@users.noreply.github.com>
2023-07-02 21:28:48 -0500

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,15 +2,11 @@
 #importar librerias para scraping
 #importamos las librerias necesarias
 import pandas as pd
-#usamos una matriz dispersa para ahorrar memoria
-from scipy.sparse import csr_matrix
 
 import streamlit as st
 #guardar anime_features_df_matrix en un archivo para usarlo en el notebook de recomendacion
 from scipy import sparse
 import sklearn
-# importamos la libreria para el modelo de recomendacion
-from sklearn.neighbors import NearestNeighbors
 #guardar el modelo para usarlo en el futuro
 import pickle
 
